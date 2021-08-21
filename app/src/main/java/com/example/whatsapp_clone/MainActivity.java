f8828140ca28195d135eb81b1d7fb85820ea179d
Riyad36 <almuhafiz@iut-dhaka.edu>
2021-08-22 02:18:44 +0600

    tool created

--- app/src/main/java/com/example/whatsapp_clone/MainActivity.java
+++ app/src/main/java/com/example/whatsapp_clone/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.whatsapp_clone;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mToolbar = findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("WhatsApp");
+
+
+
     }
+
+
 }
