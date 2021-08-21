13349233365958a8fd27d9c76c68a02e8a75c317
Riyad36 <almuhafiz@iut-dhaka.edu>
2021-08-22 03:03:47 +0600

    different fragments added

--- app/src/main/java/com/example/whatsapp_clone/MainActivity.java
+++ app/src/main/java/com/example/whatsapp_clone/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.whatsapp_clone;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private Toolbar mToolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private TabsAccessorAdapter tabsAccessorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +23,15 @@ public class MainActivity extends AppCompatActivity {
 
         mToolbar = findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);
-        getSupportActionBar().setTitle("WhatsApp");
+        getSupportActionBar().setTitle("Amra Amrai to");
+
+
+        viewPager = (ViewPager) findViewById(R.id.main_tabs_pager);
+        tabsAccessorAdapter = new TabsAccessorAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabsAccessorAdapter);
+
+        tabLayout = (TabLayout) findViewById(R.id.main_page_tabs);
+        tabLayout.setupWithViewPager(viewPager);
 
 
 
