31a80b1661da52fea8585afb9760c16be135b7c5
Riyad36 <almuhafiz@iut-dhaka.edu>
2021-08-22 09:52:42 +0600

    sign up part complete

--- app/src/main/java/com/example/whatsapp_clone/LoginActivity.java
+++ app/src/main/java/com/example/whatsapp_clone/LoginActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
 
@@ -11,12 +15,40 @@ public class LoginActivity extends AppCompatActivity {
 
 
     private FirebaseUser currentUser;
+    private Button btn_login, btn_login_with_phn;
+    private EditText et_login_email, et_login_password;
+    private TextView tv_forget_password, tv_have_no_account;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        InitializeFields();
+
+        tv_have_no_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendUserToLRegisterActivity();
+            }
+        });
     }
+
+
+
+    private void InitializeFields() {
+        btn_login = findViewById(R.id.login_btn);
+        btn_login_with_phn = findViewById(R.id.login_using_phn);
+        et_login_email = findViewById(R.id.login_email);
+        et_login_password = findViewById(R.id.login_password);
+        tv_forget_password = findViewById(R.id.forget_password_link);
+        tv_have_no_account = findViewById(R.id.do_not_have_account_link);
+        
+
+    }
+
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -28,6 +60,11 @@ public class LoginActivity extends AppCompatActivity {
     }
     private void sendUserToLMainActivity() {
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    private void sendUserToLRegisterActivity() {
+        Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/whatsapp_clone/RegisterActivity.java
+++ app/src/main/java/com/example/whatsapp_clone/RegisterActivity.java
@@ -1,14 +1,105 @@
 package com.example.whatsapp_clone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseUser currentUser;
+    private Button btn_createAccount;
+    private EditText et_register_email, et_register_password;
+    private TextView tv_already_have_account;
+
+    private FirebaseAuth mAuth;
+    private ProgressDialog loading_bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        InitializeFields();
+
+        tv_already_have_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendUserToLLoginActivity();
+            }
+        });
+
+        btn_createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CreateNewAccount();
+            }
+        });
+    }
+
+    private void CreateNewAccount() {
+        String email = et_register_email.getText().toString();
+        String password = et_register_password.getText().toString();
+
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this, "Please enter your email", Toast.LENGTH_SHORT).show();
+        }
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Please enter your password", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            loading_bar.setTitle("Creating New Account");
+            loading_bar.setMessage("Please Wait...");
+            loading_bar.setCanceledOnTouchOutside(true);
+            loading_bar.show();
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if(task.isSuccessful()){
+                                loading_bar.dismiss();
+                                Toast.makeText(RegisterActivity.this, "Account Created Successfully",Toast.LENGTH_LONG).show();
+                                sendUserToLLoginActivity();
+                            }
+                            else{
+                                String message = task.getException().toString();
+                                loading_bar.dismiss();
+                                Toast.makeText(RegisterActivity.this, "Error : " +message,Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+        }
+    }
+
+
+    private void InitializeFields() {
+        btn_createAccount = findViewById(R.id.register_btn);
+        et_register_email = findViewById(R.id.register_email);
+        et_register_password = findViewById(R.id.register_password);
+        tv_already_have_account = findViewById(R.id.already_have_account_link);
+        loading_bar = new ProgressDialog(this);
+    }
+
+    private void sendUserToLLoginActivity() {
+        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
