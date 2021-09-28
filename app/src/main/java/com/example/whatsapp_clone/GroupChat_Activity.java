f1c3b6d94fdb33ebeefc1ce55b92794ad2f1f0e3
Riyad36 <almuhafiz@iut-dhaka.edu>
2021-09-28 09:47:06 +0600

    can send message to any group

--- app/src/main/java/com/example/whatsapp_clone/GroupChat_Activity.java
+++ app/src/main/java/com/example/whatsapp_clone/GroupChat_Activity.java
@@ -1,13 +1,29 @@
 package com.example.whatsapp_clone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class GroupChat_Activity extends AppCompatActivity {
 
@@ -18,17 +34,70 @@ public class GroupChat_Activity extends AppCompatActivity {
     private EditText userMessageInput;
     private ImageButton sendMessageButton;
 
-    private String currentGroupName;
+    private FirebaseAuth mAuth;
+    private DatabaseReference UsersRef, GroupNameRef, GroupMessageKeyRef;
+
+    private String currentGroupName, currentUserName, currentUserId, currentDate, currentTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_chat_);
 
+
         currentGroupName = getIntent().getExtras().get("groupName").toString();
 
+        mAuth = FirebaseAuth.getInstance();
+        currentUserId = mAuth.getCurrentUser().getUid();
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        GroupNameRef = FirebaseDatabase.getInstance().getReference().child("Groups").child(currentGroupName);
+
         Initialize();
+        GetUserInfo();
+
+        sendMessageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SendMessageInfoToDatabase();
+
+                userMessageInput.setText("");
+            }
+        });
     }
+
+    private void SendMessageInfoToDatabase() {
+        String message = userMessageInput.getText().toString();
+        String messageKey = GroupNameRef.push().getKey();
+
+        if(TextUtils.isEmpty(message)){
+            Toast.makeText(this, "Please Write something...",Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Calendar calFordate = Calendar.getInstance();
+            SimpleDateFormat currentDateFormat = new SimpleDateFormat("MMM dd, yyyy");
+            currentDate = currentDateFormat.format(calFordate.getTime());
+
+            Calendar calForTime = Calendar.getInstance();
+            SimpleDateFormat currentTimeFormat = new SimpleDateFormat("hh:mm a");
+            currentTime = currentTimeFormat.format(calForTime.getTime());
+
+            HashMap<String, Object> groupMessageKey = new HashMap<>();
+            GroupNameRef.updateChildren(groupMessageKey);
+
+            GroupMessageKeyRef = GroupNameRef.child(messageKey);
+
+            HashMap<String, Object> messageInfoMap = new HashMap<>();
+
+            messageInfoMap.put("name", currentUserName);
+            messageInfoMap.put("message", message);
+            messageInfoMap.put("date", currentDate);
+            messageInfoMap.put("time", currentTime);
+
+            GroupMessageKeyRef.updateChildren(messageInfoMap);
+
+        }
+    }
+
 
     private void Initialize() {
         mToolbar = findViewById(R.id.group_chat_bar_layout);
@@ -39,5 +108,25 @@ public class GroupChat_Activity extends AppCompatActivity {
         displayMessage = findViewById(R.id.group_chat_text_display);
         userMessageInput = findViewById(R.id.input_group_message);
         sendMessageButton = findViewById(R.id.send_message_button);
+    }
+
+    private void GetUserInfo() {
+
+        UsersRef.child(currentUserId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    currentUserName = snapshot.child("name").getValue().toString();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+
+
+            }
+        });
     }
 }
